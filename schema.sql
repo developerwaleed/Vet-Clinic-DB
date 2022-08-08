eee84f044d6fbe37070641e6bac2464f3a5c7c9c
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-08 22:23:50 +0500

    Creat DB and Table with columns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
-/* Database schema to keep the structure of entire database. */
+/* Create a database named vet_clinic. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE DATABASE vet_clinic;
+
+/* Create a table named animals with the columns */
+
+CREATE TABLE animals ( id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL(4,2), PRIMARY KEY(id));
