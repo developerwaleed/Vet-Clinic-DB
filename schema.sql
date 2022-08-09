318575b061614cef2e2e5602738a555efd360a62
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-09 18:40:17 +0500

    Add queries and Schema.sql

--- schema.sql
+++ schema.sql
@@ -4,4 +4,17 @@ CREATE DATABASE vet_clinic;
 
 /* Create a table named animals with the columns */
 
-CREATE TABLE animals ( id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL(4,2), PRIMARY KEY(id));
+CREATE TABLE animals 
+( id INT GENERATED ALWAYS AS IDENTITY, 
+    name VARCHAR(250), 
+    date_of_birth DATE, 
+    escape_attempts INT, 
+    neutered BOOLEAN, 
+    weight_kg DECIMAL(4,2), 
+    PRIMARY KEY(id));
+
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(250);
+
+
+
