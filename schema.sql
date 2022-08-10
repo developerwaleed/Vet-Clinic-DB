a0d8ecad17f2240c79491860175bcbd457b2655b
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-10 22:55:31 +0500

    Add schema.sql

--- schema.sql
+++ schema.sql
@@ -16,5 +16,30 @@ CREATE TABLE animals
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(250);
 
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    full_name VARCHAR(250), 
+    age INT, PRIMARY KEY (id));
 
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250), 
+    PRIMARY KEY (id));
+
+ALTER TABLE animals 
+DROP COLUMN species;
+
+ALTER TABLE animals 
+ADD COLUMN species_id INT, 
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals 
+ADD CONSTRAINT fk_species 
+FOREIGN KEY(species_id) 
+REFERENCES species(id);
+
+ALTER TABLE animals 
+ADD CONSTRAINT fk_owner 
+FOREIGN KEY(owner_id)
+REFERENCES owners(id);
 
